75ea0d59b7c2aa101e02990af836e057498c4f6a
RONNY <mboyaronny41@gmail.com>
2026-02-22 17:16:19 +0300

    updating userRole.ts

--- src/hooks/useAdminRole.ts
+++ src/hooks/useAdminRole.ts
@@ -1,6 +1,6 @@
 // Stub hook - Supabase removed
 // TODO: Replace with real auth logic later
-export const useAdminRole = (user: any) => {
+export const useAdminRole = (user: unknown) => {
   return {
     isAdmin: false,
     checkingRole: false,
